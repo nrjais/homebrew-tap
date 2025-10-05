c1ef7043cda3c9dc9a487e0d5409b7cc10692e77
Neeraj Jaiswal <neerajj85@gmail.com>
2025-10-05 17:04:20 +0530

    fix lints
    Closes #2.
    
    Signed-off-by: Neeraj Jaiswal <20153307+nrjais@users.noreply.github.com>

--- Casks/sanchaar.rb
+++ Casks/sanchaar.rb
@@ -3,10 +3,12 @@ cask "sanchaar" do
 
   on_arm do
     sha256 "c886aaf290b4596de7f3458b9e36c1eb84c0df525ba4fa00fe6360ea76cc93dc"
+
     url "https://github.com/nrjais/sanchaar/releases/download/#{version}/Sanchaar-#{version}-aarch64-apple-darwin.dmg"
   end
   on_intel do
     sha256 "97869a36094147c20e33025d991bba344237a9a39684adad2b3723c797cad112"
+
     url "https://github.com/nrjais/sanchaar/releases/download/#{version}/Sanchaar-#{version}-x86_64-apple-darwin.dmg"
   end
 
